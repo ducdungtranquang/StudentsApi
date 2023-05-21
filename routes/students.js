355091223ad0ed614b772ae8f75d1ca578b2ea98
DungTQD <dungtqd@bravebits.vn>
2023-05-21 17:27:07 +0700

    first commit

---
+++ routes/students.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const Student = require('../model/student');
+
+// Get all students
+router.get('/', async (req, res) => {
+  try {
+    const students = await Student.find();
+    res.json(students);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Create a new student
+router.post('/', async (req, res) => {
+  const student = new Student(req.body);
+  try {
+    const newStudent = await student.save();
+    res.status(201).json(newStudent);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+module.exports = router;
